e52c2c20da42e846112ad22b780b5ec44c822739
Christian Perez <perezchristian1012@gmail.com>
2020-01-20 18:20:55 +0000

    Automatically backed up by Learn

--- db/migrate/20200120180705_create_users.rb
+++ db/migrate/20200120180705_create_users.rb
@@ -2,10 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :password
-      t.string :password_confirmation
+      t.string :password_digest
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
